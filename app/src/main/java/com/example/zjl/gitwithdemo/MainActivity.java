8a9d2ef7064dc18a1b274dd371a0e7d10715cda9
zjl7577 <1097919195@qq.com>
2018-08-15 09:54:44 +0800

    OK

--- app/src/main/java/com/example/zjl/gitwithdemo/MainActivity.java
+++ app/src/main/java/com/example/zjl/gitwithdemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.zjl.gitwithdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        initListener();
+    }
+
+    private void initListener() {
+        findViewById(R.id.request).setOnClickListener(v -> {
+            Toast.makeText(this,"点击了",Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    private void initView() {
     }
 }
